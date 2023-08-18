cea3710f03521189e5d3f239bc3f46a380198b76
theVerySharpFlat <theVerySharpFlat@users.noreply.github.com>
2023-08-18 22:00:34 +0000

    deploy: 7058ecb6ab1502d4a18036367d4cb03daf0d7bee

---
+++ assets/PresenterPrint-3a74f382.js
@@ -0,0 +1,17 @@
+import{d as _,i as d,a as h,u as p,b as u,c as m,e as f,f as n,g as t,t as s,h as a,F as g,r as v,n as x,j as y,o as r,k as b,l as k,m as N,p as P,q as w,_ as D}from"./index-6ee5487c.js";import{N as S}from"./NoteDisplay-0244015b.js";const V={class:"m-4"},j={class:"mb-10"},L={class:"text-4xl font-bold mt-2"},T={class:"opacity-50"},B={class:"text-lg"},C={class:"font-bold flex gap-2"},H={class:"opacity-50"},W=t("div",{class:"flex-auto"},null,-1),z={key:0,class:"border-gray-400/50 mb-8"},F=_({__name:"PresenterPrint",setup(G){d(h),p(`
+@page {
+  size: A4;
+  margin-top: 1.5cm;
+  margin-bottom: 1cm;
+}
+* {
+  -webkit-print-color-adjust: exact;
+}
+html,
+html body,
+html #app,
+html #page-root {
+  height: auto;
+  overflow: auto !important;
+}
+`),u({title:`Notes - ${m.title}`});const l=f(()=>y.slice(0,-1).map(o=>{var i;return(i=o.meta)==null?void 0:i.slide}).filter(o=>o!==void 0&&o.noteHTML!==""));return(o,i)=>(r(),n("div",{id:"page-root",style:x(a(w))},[t("div",V,[t("div",j,[t("h1",L,s(a(m).title),1),t("div",T,s(new Date().toLocaleString()),1)]),(r(!0),n(g,null,v(a(l),(e,c)=>(r(),n("div",{key:c,class:"flex flex-col gap-4 break-inside-avoid-page"},[t("div",null,[t("h2",B,[t("div",C,[t("div",H,s(e==null?void 0:e.no)+"/"+s(a(b)),1),k(" "+s(e==null?void 0:e.title)+" ",1),W])]),N(S,{"note-html":e.noteHTML,class:"max-w-full"},null,8,["note-html"])]),c<a(l).length-1?(r(),n("hr",z)):P("v-if",!0)]))),128))])],4))}}),A=D(F,[["__file","/home/runner/work/GameDevWithPython/GameDevWithPython/node_modules/@slidev/client/internals/PresenterPrint.vue"]]);export{A as default};
